c84b8454ad2045cc076e22683bbb7d99759fc28c
opiyobrayan <brayanopiyo18@gmail.com>
2022-07-27 06:52:12 +0300

    more messages

--- office_system/activities/admin.py
+++ office_system/activities/admin.py
@@ -1,3 +1,19 @@
 from django.contrib import admin
+from . models  import Store
+from . models  import Transport
+from . models  import Ticketing
+from . models  import Hotel
 
 # Register your models here.
+
+# admin.site.register(Store)
+admin.site.register(Transport)
+admin.site.register(Hotel)
+admin.site.register(Ticketing)
+
+@admin.register(Store)
+class StoreAdmin(admin.ModelAdmin):
+    list_display = ('name','quantity','amount','date')
+    ordering =  ('date',)
+    search_fields = ('name','amount')
+    list_filter = ('amount','quantity')
--- office_system/activities/models.py
+++ office_system/activities/models.py
@@ -1,3 +1,45 @@
 from django.db import models
 
+
 # Create your models here.
+
+class Store(models.Model):
+    name = models.CharField('Product Name', max_length=120)
+    quantity = models.IntegerField('quantity', max_length=120, blank=True)
+    amount = models.IntegerField('Amount', max_length=120)
+    status = models.TextChoices( 'Added','Used')
+    date= models.DateTimeField('Date')
+    descriprion= models.TextField('description', blank=True)
+
+    def __str__(self):
+        return self.name
+
+class Transport(models.Model):
+    name = models.CharField('Name of Company', max_length=120)
+    contact = models.IntegerField('Contact Person', max_length=120)
+    mobile = models.IntegerField('Telephone number', max_length=120)
+    email= models.EmailField('Email Address')
+    services = models.CharField('services', max_length=120)
+    def __str__(self):
+        return self.name
+
+
+class Hotel(models.Model):
+    name = models.CharField('Name of Hotel', max_length=120)
+    accommadate= models.CharField('accommadate', max_length=120)
+    confernce = models.CharField('conference', max_length=120)
+    contact_name = models.CharField('contact name', max_length=120)
+    contact_mobile =  models.CharField('contact mobile', max_length=120)
+    email= models.EmailField('Email Address')
+
+    def __str__(self):
+        return self.name
+
+class Ticketing(models.Model):
+    name = models.CharField('Name of Company', max_length=120)
+    contact = models.IntegerField('Contact Person', max_length=120)
+    mobile = models.IntegerField('Telephone number', max_length=120)
+    email= models.EmailField('Email Address')
+    services = models.CharField('services', max_length=120)
+    def __str__(self):
+        return self.name

f1d0ad9d7d033414199d24590434ddf36390e0a0
opiyobrayan <brayanopiyo18@gmail.com>
2022-07-28 12:52:32 +0300

    updting to site

--- office_system/activities/views.py
+++ office_system/activities/views.py
@@ -1,7 +1,12 @@
 from django.shortcuts import render
-from . models import Store
+from .models import Store
 # Create your views here.
 def home(request):
-    stores= Store.objects.all()
-    return render(request,'home.html',{'stores':stores})
+    return render(request,'home.html',{})
 
+# displaying items
+
+def store(request):
+    store_list=Store.objects.all()
+
+    return render(request, 'store.html',{'store_list':store_list})
